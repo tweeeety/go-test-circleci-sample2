8bd87c595bbfd93dec467c6281e72ac3a4e4c104
tweeeety <tweeeety.0719@gmail.com>
2018-03-13 21:43:49 +0900

    add test

--- sample01_test.go
+++ sample01_test.go
@@ -1,7 +1,9 @@
 package sample01
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestHelloWorld(t *testing.T) {
@@ -10,4 +12,26 @@ func TestHelloWorld(t *testing.T) {
 	if actual != expected {
 		t.Errorf("actual %v\nwant %v", actual, expected)
 	}
+}
+
+type Cases []Case
+type Case struct {
+	in, want string
+}
+
+func TestTableSample(t *testing.T) {
+	c1 := Case{"hoge", "egoh"}
+	c2 := Case{"fuga", "aguf"}
+
+	cs := Cases{}
+
+	for i := 0; i < 100; i++ {
+		cs = append(cs, c1)
+		cs = append(cs, c2)
+
+		fmt.Printf("loop %d\n", i+1)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("%+v", cs)
 }
